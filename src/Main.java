b211c57e168c5663ebcf94ac4c729b0bfd01bfc8
david <david.0715@outlook.de>
2025-05-22 17:38:13 +0200

    reverted Main changes

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,3 @@
-import model.Interval;
-import model.Note;
-
 public class Main { 
-    public static void main(String[] args) {
 
-        Note note = Note.getNoteFromInterval(Note.C5, Interval.P4, true);
-        System.out.println(note.getName());
-        
-    }
 }
