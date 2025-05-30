b3cf853353844fb300160ab70d48dce8cff0af71
InaasHammoush <121546232+InaasHammoush@users.noreply.github.com>
2025-05-31 00:08:49 +0200

    just some missing comments

--- src/manager/LevelBuilder.java
+++ src/manager/LevelBuilder.java
@@ -35,14 +35,14 @@ public class LevelBuilder {
         switch (levelInfo.getMode()) {
             case NOTE:
                 note = NoteUtil.getRandomNoteInRange(levelInfo.getDifficulty().getDifficultyRange()); 
-                referenceNotes.add(note);
+                referenceNotes.add(note); // a function to convert Note to MidiNote is still needed
                 return referenceNotes;
             case INTERVAL:
-                // Generate a random note and an interval based on the difficulty
+                // Generate a random note and interval based on difficulty
                 note = NoteUtil.getRandomNoteInRange(levelInfo.getDifficulty().getDifficultyRange());
                 // Here the logic seems to be faulty, maybe a range should be passed instead of interval
                 Interval interval = IntervalUtil.getRandomIntervalInRange(null, null); 
-                referenceNotes.add(NoteUtil.getNoteFromInterval(note, interval));
+                referenceNotes.add(NoteUtil.getNoteFromInterval(note, interval)); // a function to convert Note to MidiNote is still needed
                 return referenceNotes;
             case MELODY:
                 // Here we can get a random melody from a predefined pool
