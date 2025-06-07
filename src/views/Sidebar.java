435c1b23cfd52370c16c3c2c7175e934a1c3a7fa
InaasHammoush <121546232+InaasHammoush@users.noreply.github.com>
2025-06-07 16:24:26 +0200

    fixed Emojis

--- src/views/Sidebar.java
+++ src/views/Sidebar.java
@@ -28,7 +28,8 @@ public class Sidebar extends JPanel {
         add(progressButton);
         add(Box.createVerticalGlue());
 
-        ModernButton backToMenu = new ModernButton("⟵ " + LanguageManager.get("sidebar.menu"));
+        ModernButton backToMenu = new ModernButton("⬅️ " + LanguageManager.get("sidebar.menu"));
+        backToMenu.setFont(new Font("Segoe UI Emoji", Font.BOLD, 14));
         backToMenu.addActionListener(e -> controller.showHome());
         add(backToMenu);
     }
