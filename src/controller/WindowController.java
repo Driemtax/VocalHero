b3191f0bc4f5b93535065e3dc7f263a4dc0c6fb7
InaasHammoush <121546232+InaasHammoush@users.noreply.github.com>
2025-06-07 12:59:27 +0200

    remove comment

--- src/controller/WindowController.java
+++ src/controller/WindowController.java
@@ -127,7 +127,6 @@ public class WindowController extends JFrame{
         contentPanel.repaint();
     }
 
-    // TODO: Remove since it is not used anywhere
     public void showLevelSelection(Mode mode) {
         contentPanel.removeAll();
         try {
