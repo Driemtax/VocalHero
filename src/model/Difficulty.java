57d29c1282b27da5385189342019176ea1e2e4bf
InaasHammoush <121546232+InaasHammoush@users.noreply.github.com>
2025-06-08 14:44:11 +0200

    make difficulty range bigger

--- src/model/Difficulty.java
+++ src/model/Difficulty.java
@@ -10,9 +10,9 @@ public enum Difficulty{
     MidiNote.Note baseUserNote = MidiNote.Note.C4; //TODO get the actual base Note 
 
         return switch(this) {
-            case EASY -> new Range(baseUserNote.ordinal() - 1, baseUserNote.ordinal() + 1);
-            case MEDIUM -> new Range(baseUserNote.ordinal() - 2, baseUserNote.ordinal() + 2);
-            case HARD -> new Range(baseUserNote.ordinal() - 3, baseUserNote.ordinal() + 3); //TODO adjust values to fit
+            case EASY -> new Range(baseUserNote.ordinal() - 3, baseUserNote.ordinal() + 3);
+            case MEDIUM -> new Range(baseUserNote.ordinal() - 5, baseUserNote.ordinal() + 5);
+            case HARD -> new Range(baseUserNote.ordinal() - 8, baseUserNote.ordinal() + 8); //TODO adjust values to fit
         };
     }
 }
